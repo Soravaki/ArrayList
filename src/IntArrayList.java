6cf84c40e4ccc4125e6ec63068054dd99fa425cc
Soravaki <92048405+Soravaki@users.noreply.github.com>
2021-11-05 13:03:04 -0500

    School Work

--- src/IntArrayList.java
+++ src/IntArrayList.java
@@ -1,54 +1,73 @@
 public class IntArrayList {
-    private int elements, size;
+    private int elements;
     private int[] arraylist;
+
     // construct empty list
     public IntArrayList(){
         arraylist = new int[5];
         elements = 0;
-        size = arraylist.length;
     }
+
     //add element to end of list
-    public boolean add(int element){
-        if ( size <= element){
-            expandlist();
-        }
+    public void add(int element){
+        expandlist();
         arraylist[elements] = element;
+        elements++;
+    }
+
+    // insert element at index
+    public boolean insert(int element, int index){
+        if (index>elements) return false;
+        expandlist();
+        for (int i=elements; i>=index; i--){
+            arraylist[i+1] = arraylist[i];
+        }
+        arraylist[index] = element;
         elements++;
         return true;
     }
-    // insert element at index
-    public boolean insert(int element, int index){
-        if ( size <= element){
-            expandlist();
-        }
-        for (int i=index; i<arraylist.length; i++){
-            int temp = arraylist[i+1];
-            arraylist[i+1] = arraylist[i];
-        }
-        return true;
-    }
+
     // remove element at index
     public boolean remove(int index){
+        if (index>elements) return false;
+        for (int i=index; i<elements; i++){
+            arraylist[i] = arraylist[i+1];
+        }
+        arraylist[elements] = 0;
+        elements--;
         return true;
     }
+
     // return true if list contains element
     public boolean find(int element){
-        return true;
+        for (int i=0; i<elements; i++){
+            if (arraylist[i] == element) return true;
+        }
+        return false;
     }
+
     // returns the number of elements in list
     public int size(){
         return elements;
     }
+
     // expands arraylist by 5
     public void expandlist(){
-        int[] templist = new int[size+5];
-        for (int i=0; i<arraylist.length; i++){
-            templist[i] = arraylist[i];
+        if ( arraylist.length <= elements){
+            int[] templist = new int[arraylist.length+5];
+            for (int i=0; i<arraylist.length; i++){
+                templist[i] = arraylist[i];
+            }
+            arraylist = templist;
         }
-        arraylist = templist;
     }
+
     // print the list
     public String toString(){
-        return "";
+        String output = "";
+        for (int i=0; i<elements; i++){
+            output += "" + arraylist[i] + " ";
+        }
+        return output;
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -2,9 +2,22 @@ public class Runner {
     public static void main(String[] args) {
         IntArrayList m = new IntArrayList();
         System.out.println(m.size());
-        for (int i=0; i<10; i++){
-            m.add(i);
+        for (int i=0; i<8; i++){
+            if (i > 4) {
+                m.add(i+1);
+            }
+            else m.add(i);
+
         }
+        m.remove(20);
+        m.insert(20, 20);
         System.out.println(m.size());
+        System.out.println(m);
+        System.out.println(m.find(5));
+        m.insert(5, 5);
+        System.out.println(m);
+        System.out.println(m.find(5));
+        m.remove(5);
+        System.out.println(m);
     }
 }
